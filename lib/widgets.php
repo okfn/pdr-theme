fd01682206c2c6e56f31734293e9c44daf0331ec
Bobby Donovan <bobby.donovan@gmail.com>
2013-12-02 17:13:05 -0500

    Working

--- lib/widgets.php
+++ lib/widgets.php
@@ -3,36 +3,45 @@
  * Register sidebars and widgets
  */
 function roots_widgets_init() {
-    // Sidebars
-    register_sidebar(array(
-        'name'          => __('Left Sidebar', 'roots'),
-        'id'            => 'sidebar-left',
-        'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
-        'after_widget'  => '</div></section>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>',
-    ));
+   // Sidebars
+   register_sidebar(array(
+      'name'          => __('Left Sidebar', 'roots'),
+      'id'            => 'sidebar-left',
+      'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+      'after_widget'  => '</div></section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>',
+   ));
 
-    register_sidebar(array(
-        'name'          => __('Right Sidebar', 'roots'),
-        'id'            => 'sidebar-right',
-        'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
-        'after_widget'  => '</div></section>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>',
-    ));
+   register_sidebar(array(
+      'name'          => __('Right Sidebar', 'roots'),
+      'id'            => 'sidebar-right',
+      'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+      'after_widget'  => '</div></section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>',
+   ));
 
-  register_sidebar(array(
-    'name'          => __('Footer', 'roots'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
-    'after_widget'  => '</div></section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ));
+   register_sidebar(array(
+      'name'          => __('Footer Top', 'roots'),
+      'id'            => 'sidebar-footer-top',
+      'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+      'after_widget'  => '</div></section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>',
+   ));
+
+   register_sidebar(array(
+      'name'          => __('Footer Bottom', 'roots'),
+      'id'            => 'sidebar-footer-bottom',
+      'before_widget' => '<section class="widget %1$s %2$s"><div class="widget-inner">',
+      'after_widget'  => '</div></section>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>',
+   ));
 
   // Widgets
-  register_widget('Roots_Vcard_Widget');
+  // register_widget('Roots_Vcard_Widget');
 }
 add_action('widgets_init', 'roots_widgets_init');
 
--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,20 @@
 <footer class="content-info container" role="contentinfo">
 	<div class="row">
-		<div class="col-lg-12">
-			<p>Footer</p>
-			  <?php dynamic_sidebar('sidebar-footer'); ?>
-			  <!-- <p>&copy; <?php// echo date('Y'); ?> <?php bloginfo('name'); ?></p> -->
+		<?php if ( is_active_sidebar('sidebar-footer-top') ): ?>
+		<div class="footer-top">
+			<div class="inner">
+				<?php dynamic_sidebar('sidebar-footer-top'); ?>
+			</div>
 		</div>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar('sidebar-footer-bottom') ): ?>
+		<div class="footer-bottom">
+			<div class="inner">
+				<?php dynamic_sidebar('sidebar-footer-bottom'); ?>
+			</div>
+		</div>
+		<?php endif; ?>
 	</div>
 </footer>
 
