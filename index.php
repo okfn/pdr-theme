cf2047e8531333b58daa1e3fda4c5f17b92a237e
Bobby Donovan <bobby.donovan@gmail.com>
2013-12-02 17:33:45 -0500

    Show for all

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
         <?php get_template_part('templates/content', $type = (get_post_type() == 'post' ) ? '' : 'collection' ); ?>
     <?php endwhile; ?>
     
-    <?php if ($wp_query->max_num_pages > 1) :  ?>
+    <?php //if ($wp_query->max_num_pages > 1) :  ?>
         <div class="archive-link-container">
             <a class="archive-link" href="<?php echo add_query_arg('medium', get_query_var('medium'), get_post_type_archive_link( $wp_query->query['post_type'] ) ) ?>">See more</a>
         </div>
-    <?php endif; ?>
+    <?php //endif; ?>
 </div>
