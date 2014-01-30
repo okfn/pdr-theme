99c1d54452edf53a991748488b8d3bdadb99b19f
Bobby Donovan <bobby.donovan@gmail.com>
2014-01-30 11:39:00 -0500

    Better height fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,32 @@
 (function($) {
 
-	function homepage_box_heights() {
-		if( Modernizr.mq('screen and (min-width:768px)') ) {
-			$('.home-collection .row').each(function(){
-				$(this).find('.regular-list-item .article-inner').equalHeights();
-			});
-		}
-	}
+	function fix_heights() {
 
-	function collections_box_heights() {
 		if( Modernizr.mq('screen and (min-width:768px)') ) {
-			$('.grid .main .regular-list-item, .related-content article').equalHeights();
-		}
-	}
+			if ( $('.home-collection .row').length ) {
+				$('.home-collection .row').each(function(){
+					$(this).find('.regular-list-item .article-inner').equalHeights();
+				});
+			}
 
-	function collections_sidebar_heights_desktop() {
-		if( Modernizr.mq('screen and (min-width:768px)') ) {
-			$('.home-collection').each(function(){
-				$(this).find('.main, .sidebar, .sidebar .widget-inner').equalHeights();
-			});
-		}
-	}
+			if ( $('.grid .main .regular-list-item, .related-content article').length ) {
+				$('.grid .main .regular-list-item, .related-content article').equalHeights();
+			}
 
-	function collections_sidebar_heights_mobile() {
-		if( Modernizr.mq('screen and (max-width:767px)') ) {
-			$('.home-collection').each(function(){
-				$(this).find('.sidebar, .sidebar .widget-inner').equalHeights();
-			});
+			if ( $('.home-collection .row').length ) {
+				$('.home-collection').each(function(){
+					$(this).find('.main, .sidebar, .sidebar .widget-inner').equalHeights();
+				});
+			}
+
+
+			if( Modernizr.mq('screen and (max-width:767px)') ) {
+				if ( $('.home-collection').length ) {
+					$('.home-collection').each(function(){
+						$(this).find('.sidebar, .sidebar .widget-inner').equalHeights();
+					});
+				}
+			}
 		}
 	}
 
@@ -41,17 +41,14 @@
 	var id;
     $(window).resize(function() {
         clearTimeout(id);
-        id = setTimeout(homepage_box_heights, 0);
-        id = setTimeout(collections_box_heights, 0);
-        id = setTimeout(collections_sidebar_heights_desktop, 0);
-        id = setTimeout(collections_sidebar_heights_mobile, 0);
-        
+        id = setTimeout(fix_heights, 0);
     });
 
-    homepage_box_heights();
-    collections_box_heights();
-    collections_sidebar_heights_desktop();
-    collections_sidebar_heights_mobile();
+    $(window).load(function() {
+		fix_heights();
+    });
+
+    fix_heights();
 	
 })(jQuery);
 
