0c52d1d03571ebcd6f8cc3aaec7d228344251e89
Bobby Donovan <bobby.donovan@gmail.com>
2014-02-06 11:56:52 -0500

    Remove search placeholder

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
-	<input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field " placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+	<input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field">
 	<label class="hide"><?php _e('Search for:', 'roots'); ?></label>
 	<button type="submit" class="search-submit"><?php _e('Search', 'roots'); ?></button>
 </form>
